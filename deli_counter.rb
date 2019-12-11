213b89607349de4a39fedb1f7c8371328f88a5b7
Joanna E K <kalemajoanna@gmail.com>
2019-12-11 18:12:07 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,7 @@
 # Write your code here.
   katz_deli = []
 def line(array) 
- if array== 0 
+ if array.size == 0 
     puts  "The line is currently empty."
   else array > 0 
     message = "The line is currently: "
