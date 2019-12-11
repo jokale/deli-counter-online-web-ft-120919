9321461275c1a0eb307f22b88465600fc84663e3
Joanna E K <kalemajoanna@gmail.com>
2019-12-11 17:58:30 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,19 +4,7 @@ def line
   katz_deli = []
   if katz_deli== 0 
     puts  "The line is currently empty."
-#   else katz_deli > 0 
-#     puts "The line is currently "
-#   puts katz_deli
-# else 
-#   puts 
 end 
+
+def take_a_number(katz_deli, string)
 end 
-# string = "The line is currently: "
-#     katz_deli.each_with_index do |name, index|	    katz_deli.each_with_index do |name, index|
-
-
-#     puts	    string << ("#{index+1}. #{name} ")
-#     end	    end
-#     puts string.strip
-#   end	  end
-# end 	end
