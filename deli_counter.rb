6e92d9145a7c563fb8e36ea9fafdc822f3454e3a
Joanna E K <kalemajoanna@gmail.com>
2019-12-11 18:21:26 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,7 +19,6 @@ end
 
 def now_serving(katz_deli)
   if katz_deli.size == 0
-     katz_deli.push(name)
     puts "There is nobody waiting to be served!"
   else
     current_customer = katz_deli.shift
