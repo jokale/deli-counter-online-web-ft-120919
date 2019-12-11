c33bdc4725ca99b594f0d146beda3d90cfc9e2f3
Joanna E K <kalemajoanna@gmail.com>
2019-12-11 18:05:06 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,8 @@
 # Write your code here.
-
-def line 
   katz_deli = []
-  if katz_deli== 0 
+def line 
+ if katz_deli== 0 
     puts  "The line is currently empty."
+  else katz_deli > 0 
+    
 end 
