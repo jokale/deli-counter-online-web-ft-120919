3a5cce21a6d96216451a579fdb724f66506da8fb
Joanna E K <kalemajoanna@gmail.com>
2019-12-11 18:10:25 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,5 +4,11 @@ def line
  if katz_deli== 0 
     puts  "The line is currently empty."
   else katz_deli > 0 
-    
+    message = "The line is currently: "
+    array.each_with_index do |name, index| 
+      message += "#{index + 1}. #{name} "
+    end
+    puts message.rstrip
+  end
+end
 end 
